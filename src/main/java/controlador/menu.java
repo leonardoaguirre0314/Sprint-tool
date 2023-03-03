bfaf012ad38a40bd3225ab0b6677e38570cedfac
Andreww80k <andresnumpaque2880@gmail.com>
2023-03-03 00:13:35 -0500

    Agregar controlador al proyecto Arraylist 3

--- src/main/java/controlador/menu.java
+++ src/main/java/controlador/menu.java
@@ -1,4 +1,24 @@
 package controlador;
 
-public class menu {
+
+@RestController
+@RequestMapping("/estudiantes")
+public class EstudianteController {
+    @Autowired
+    private EstudianteService estudianteService;
+
+    @GetMapping
+    public List<Estudiante> obtenerTodos() {
+        return estudianteService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Estudiante obtenerPorId(@PathVariable Long id) {
+        return estudianteService.obtenerPorId(id);
+    }
+
+    @PostMapping
+    public void guardar(@RequestBody Estudiante estudiante) {
+        estudianteService.guardar(estudiante);
+    }
 }
--- src/main/java/entidad/estudiante.java
+++ src/main/java/entidad/estudiante.java
@@ -1,4 +1,63 @@
 package entidad;
 
-public class estudiante {
+
+public class Estudiante {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String carrera;
+
+    // Constructor, getters y setters
+
+    public Estudiante() {
+    }
+
+    public Estudiante(Long id, String nombre, String apellido, int edad, String carrera) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
 }

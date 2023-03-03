644bfe12e9299ed6859b32eb716b4860b3b2fc9d
Andrew80k <mgiraldo594@misena.edu.co>
2023-03-03 07:50:32 -0500

    Version1.0

--- src/main/java/controlador/menu.java
+++ src/main/java/controlador/menu.java
@@ -1,23 +1,41 @@
 package controlador;
 
-@RestController
-@RequestMapping("/estudiantes")
-public class EstudianteController {
-    @Autowired
-    private EstudianteService estudianteService;
+import java.util.Scanner;
 
-    @GetMapping
-    public List<Estudiante> obtenerTodos() {
-        return estudianteService.obtenerTodos();
-    }
+public class Menu {
+    private EstudianteService estudianteService = new EstudianteService();
+    private Scanner scanner = new Scanner(System.in);
 
-    @GetMapping("/{id}")
-    public Estudiante obtenerPorId(@PathVariable Long id) {
-        return estudianteService.obtenerPorId(id);
-    }
+    public void mostrarMenu() {
+        int opcion = 0;
+        do {
+            System.out.println("\n--- MENU PRINCIPAL ---");
+            System.out.println("1. Agregar estudiante");
+            System.out.println("2. Buscar estudiante");
+            System.out.println("3. Actualizar estudiante");
+            System.out.println("4. Eliminar estudiante");
+            System.out.println("5. Listar estudiantes");
+            System.out.println("6. Salir");
+            System.out.print("Ingrese una opción: ");
 
-    @PostMapping
-    public void guardar(@RequestBody Estudiante estudiante) {
-        estudianteService.guardar(estudiante);
-    }
-}
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    agregarEstudiante();
+                    break;
+                case 2:
+                    buscarEstudiante();
+                    break;
+                case 3:
+                    actualizarEstudiante();
+                    break;
+                case 4:
+                    eliminarEstudiante();
+                    break;
+                case 5:
+                    listarEstudiantes();
+                    break;
+                case 6:
+                    System.out.println("¡Hasta luego!");
+                   

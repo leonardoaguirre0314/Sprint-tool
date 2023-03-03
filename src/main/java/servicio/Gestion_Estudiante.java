f4e9bf7d7e62fdf5122e3042ef628150a019a251
JuanFeCastell <juanfe3012@gmail.com>
2023-03-02 23:38:26 -0500

    version_GestionEstudiante2

--- src/main/java/servicio/Gestion_Estudiante.java
+++ src/main/java/servicio/Gestion_Estudiante.java
@@ -1,4 +1,30 @@
 package servicio;
 
 public class Gestion_Estudiante {
+    public class EstudianteService {
+        private List<Estudiante> estudiantes = new ArrayList<>();
+
+        public List<Estudiante> obtenerTodos() {
+            return estudiantes;
+        }
+
+        public Estudiante obtenerPorId(Long id) {
+            return estudiantes.stream()
+                    .filter(estudiante -> estudiante.getId().equals(id))
+                    .findFirst()
+                    .orElse(null);
+        }
+
+        public void guardar(Estudiante estudiante) {
+            estudiante.setId(generarId());
+            estudiantes.add(estudiante);
+        }
+
+        private Long generarId() {
+            return estudiantes.stream()
+                    .mapToLong(Estudiante::getId)
+                    .max()
+                    .orElse(0L) + 1;
+        }
+    }
 }
